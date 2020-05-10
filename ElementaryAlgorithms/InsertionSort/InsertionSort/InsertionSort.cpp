60aaa8cc950db27845ba570fac9dbe83d103d6dc
Rohit Malhotra <romalh@microsoft.com>
2020-05-10 10:11:42 -0700

    Insertion sort

--- ElementaryAlgorithms/InsertionSort/InsertionSort/InsertionSort.cpp
+++ ElementaryAlgorithms/InsertionSort/InsertionSort/InsertionSort.cpp
@@ -1,20 +1,60 @@
-// InsertionSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// InsertionSort.cpp : Basic version and Optimized version (optimized to reduce the number of comparisons, not the swaps)
 //
 
 #include <iostream>
 
-int main()
+using namespace std;
+
+void Swap(int& a, int& b)
 {
-    std::cout << "Hello World!\n";
+    int temp;
+
+    temp = a;
+    a = b;
+    b = temp;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void InsertionSortBasic(int A[], int n)
+{
+    // we're basically doing induction on the size of the array
+    // outer loop sets up the induction hypothesis 
+    // inner loop performs the inductive step
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (A[j] < A[j - 1])
+            {
+                Swap(A[j], A[j - 1]);
+            }
+        }
+    }
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void PrintArray(int A[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i] << '\t';
+    }
+}
+
+// TODO
+int BinarySearch(int A[], int low, int high, int n)
+{
+    return n;
+}
+
+// TODO
+void InsertionSortOptimized(int A[], int n)
+{
+
+}
+
+int main()
+{
+    int A[10] = { 1,10,8,15,6,7,3,4,2,9 };
+    InsertionSortBasic(A, 10);
+    PrintArray(A, 10);
+    //InsertionSortOptimized(A, 10);
+}
