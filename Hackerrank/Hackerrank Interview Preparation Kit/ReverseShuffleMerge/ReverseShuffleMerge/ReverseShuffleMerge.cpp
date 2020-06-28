6a3ba0d44eb0bdec81df57bf822ae6da2ad3be62
Rohit Malhotra <romalh@microsoft.com>
2020-06-28 14:10:21 -0700

    ReverseShuffleMerge

--- Hackerrank/Hackerrank Interview Preparation Kit/ReverseShuffleMerge/ReverseShuffleMerge/ReverseShuffleMerge.cpp
+++ Hackerrank/Hackerrank Interview Preparation Kit/ReverseShuffleMerge/ReverseShuffleMerge/ReverseShuffleMerge.cpp
@@ -1,20 +1,78 @@
-// ReverseShuffleMerge.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Refer to problem at - https://www.hackerrank.com/challenges/reverse-shuffle-merge/problem
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool buildString(vector<vector<int>>& pos, vector<int>& freq, string& ans, int low, int high, int len)
+{
+    bool isCharLeft = false;
+    
+    for (int i = 0; i < 26; i++)
+    {
+        if (freq[i] > 0)
+        {
+            isCharLeft = true;
+            if (pos[i][0] > high || pos[i][pos[i].size() - 1] < low) continue;
+            else
+            {
+                for (int j = pos[i].size() - 1; j >= 0; j--)
+                {
+                    if (pos[i][j] >= low && pos[i][j] <= high)
+                    {
+                        freq[i]--;
+                        ans.push_back(i + 'a');
+                        len--;
+                        if (buildString(pos, freq, ans, len - 1, pos[i][j] - 1, len)) return true;
+                        else
+                        {
+                            freq[i]++;
+                            ans.pop_back();
+                            len++;
+                        };
+                    }
+                }
+            }
+        }
+    }
+
+    return len == 0 && !isCharLeft;
+}
+
+// Complete the reverseShuffleMerge function below.
+string reverseShuffleMerge(string s) {
+
+    vector<int> freq(26, 0);
+    vector<vector<int>> pos(26);
+    string ans;
+    
+    for (int i = 0; i < s.size(); i++)
+    {
+        freq[s[i] - 'a']++;
+        pos[s[i] - 'a'].push_back(i);
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        freq[i] /= 2;
+    }
+
+    buildString(pos, freq, ans, s.size() / 2 - 1, s.size() - 1, s.size() / 2);
+
+    return ans;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string s;
+    getline(cin, s);
+
+    string result = reverseShuffleMerge(s);
+
+    cout << result << "\n";
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
