0e851ea63523ec61f9d558a5207da720213b7c80
Rohit Malhotra <romalh@microsoft.com>
2020-06-28 14:57:13 -0700

    revert back to using vector

--- Hackerrank/Hackerrank Interview Preparation Kit/ReverseShuffleMerge/ReverseShuffleMerge/ReverseShuffleMerge.cpp
+++ Hackerrank/Hackerrank Interview Preparation Kit/ReverseShuffleMerge/ReverseShuffleMerge/ReverseShuffleMerge.cpp
@@ -2,40 +2,34 @@
 //
 
 #include <iostream>
-#include <map>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-bool buildString(map<char, vector<int>>& pos, map<char, int>& freq, string& ans, int low, int high, int len)
+bool buildString(vector<vector<int>>& pos, vector<int>& freq, string& ans, int low, int high, int len)
 {
     bool isCharLeft = false;
-    map<char, int>::iterator itFreq;
     
-    for (itFreq = freq.begin(); itFreq != freq.end(); itFreq++)
+    for (int i = 0; i < 26; i++)
     {
-        if (itFreq->second > 0)
+        if (freq[i] > 0)
         {
             isCharLeft = true;
-
-            if (pos[itFreq->first][0] > high || pos[itFreq->first][pos[itFreq->first].size() - 1] < low) continue;
-
+            if (pos[i][0] > high || pos[i][pos[i].size() - 1] < low) continue;
             else
             {
-                for (int j = pos[itFreq->first].size() - 1; j >= 0; j--)
+                for (int j = pos[i].size() - 1; j >= 0; j--)
                 {
-                    if (pos[itFreq->first][j] >= low && pos[itFreq->first][j] <= high)
+                    if (pos[i][j] >= low && pos[i][j] <= high)
                     {
-                        itFreq->second--;
-                        ans.push_back(itFreq->first);
+                        freq[i]--;
+                        ans.push_back(i + 'a');
                         len--;
-
-                        if (buildString(pos, freq, ans, len - 1, pos[itFreq->first][j] - 1, len)) return true;
-
+                        if (buildString(pos, freq, ans, len - 1, pos[i][j] - 1, len)) return true;
                         else
                         {
-                            itFreq->second++;
+                            freq[i]++;
                             ans.pop_back();
                             len++;
                         };
@@ -51,20 +45,19 @@ bool buildString(map<char, vector<int>>& pos, map<char, int>& freq, string& ans,
 // Complete the reverseShuffleMerge function below.
 string reverseShuffleMerge(string s) {
 
-    map<char, int> freq;
-    map<char, vector<int>> pos;
-    map<char, int>::iterator itFreq;
+    vector<int> freq(26, 0);
+    vector<vector<int>> pos(26);
     string ans;
     
     for (int i = 0; i < s.size(); i++)
     {
-        freq[s[i]]++;
-        pos[s[i]].push_back(i);
+        freq[s[i] - 'a']++;
+        pos[s[i] - 'a'].push_back(i);
     }
 
-    for (itFreq = freq.begin(); itFreq != freq.end(); itFreq++)
+    for (int i = 0; i < 26; i++)
     {
-        itFreq->second /= 2;
+        freq[i] /= 2;
     }
 
     buildString(pos, freq, ans, s.size() / 2 - 1, s.size() - 1, s.size() / 2);
