caf28cacb4d8891767ab273bb7577321f31dba8d
Rohit Malhotra <romalh@microsoft.com>
2020-06-27 19:59:40 -0700

    AngryChildren SimplePrimarlitytest

---
+++ Hackerrank/Hackerrank Interview Preparation Kit/AngryChildren/AngryChildren.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Complete the maxMin function below.
+int maxMin(int k, vector<int> arr) {
+
+    sort(arr.begin(), arr.end());
+
+    int minDiff = INT_MAX;
+    for (int i = 0; i < arr.size() - k + 1; i++)
+    {
+        int diff = arr[i + k - 1] - arr[i];
+        if (diff < minDiff)
+        {
+            minDiff = diff;
+        }
+    }
+    return minDiff;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int k;
+    cin >> k;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item;
+        cin >> arr_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        arr[i] = arr_item;
+    }
+
+    int result = maxMin(k, arr);
+
+    cout << result << "\n";
+
+    return 0;
+}
---
+++ Hackerrank/Hackerrank Interview Preparation Kit/SimplePrimalityTest/SimplePrimalityTest/SimplePrimalityTest.cpp
@@ -0,0 +1,42 @@
+// Refer to problem at - https://www.hackerrank.com/challenges/ctci-big-o/problem
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+// Complete the primality function below.
+string primality(int n) {
+    if (n == 1) return "Not prime";
+    else if (n == 2 || n == 3) return "Prime";
+    else if (n % 2 == 0) return "Not prime";
+    else 
+    {
+        int root = sqrt(n);
+
+        for (int i = 3; i <= root; i = i + 2)
+        {
+            if (n % i == 0) return "Not prime";
+        }
+    }
+    return "Prime";
+}
+
+int main()
+{
+    int p;
+    cin >> p;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int p_itr = 0; p_itr < p; p_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string result = primality(n);
+
+        cout << result << "\n";
+    }
+
+    return 0;
+}
